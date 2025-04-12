862cf6c16ad721acedcf98ea92f340fbf4d780ee
Harendrasinh <haren111990@gmail.com>
2025-04-12 14:47:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div>
+        <div className='min-h-screen min-w-full bg-[url("/images/background.jpg")] bg-no-repeat bg-cover bg-center'>
+          
+        </div>
+        <footer className='h-[5rem] w-screen bg-slate-900 text-amber-200 flex flex-col justify-center items-center'>
+          <div>@2025 VayuSoftwares</div>
+          <div>The Digital Wedding Cards Developers</div>
+        </footer>
+    </div>
+  )
+}
+
