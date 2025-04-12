9706a2e0bc10726c9f9f3f25f7256a31724df24d
Harendrasinh <haren111990@gmail.com>
2025-04-12 16:01:09 +0530

    add pic

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export const App = () => {
           <div className='w-[8rem] h-[8rem] mt-8'>
             <motion.img className='w-full h-full' src="/images/ganeshji.png" alt="ganeshji" initial={{opacity:0}}
             animate={{opacity:1}}
-            transition={{duration:1,delay:2}}></motion.img>
+            transition={{duration:2}}></motion.img>
           </div>
         </div>
         <footer className='h-[5rem] w-screen bg-slate-900 text-amber-200 flex flex-col justify-center items-center'>
